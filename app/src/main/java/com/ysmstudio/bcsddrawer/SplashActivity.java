a6e91c05c7d735bcaf42e09cb3dd878201cf07d1
Seungmin Yang <tmdals099@gmail.com>
2020-01-01 20:35:08 +0900

    로그인 시스템 구현

--- app/src/main/java/com/ysmstudio/bcsddrawer/SplashActivity.java
+++ app/src/main/java/com/ysmstudio/bcsddrawer/SplashActivity.java
@@ -1,11 +1,23 @@
 package com.ysmstudio.bcsddrawer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +36,36 @@ public class SplashActivity extends AppCompatActivity {
                 SplashActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
+                        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build()
+                        );
+
+                        startActivityForResult(AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(), RC_SIGN_IN);
                     }
                 });
             }
         }).start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if(resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                assert user != null;
+                Toast.makeText(this, user.getDisplayName() + "님 안녕하세요", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
